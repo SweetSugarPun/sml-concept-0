9f11c00a9d068ab10a3eb7213e91a9e6f198e981
Arkadiusz Sugier <sugier.arkadiusz@gmail.com>
2026-05-26 19:22:28 +0200

    Polished changes

---
+++ src/data/contact.ts
@@ -0,0 +1,11 @@
+export const contact = {
+    name: 'Skup Mieszkań Lublin',
+    nip: '505-007-45-81',
+    regon: '367733735',
+    phone: '+48 516 215 523',
+    phoneHref: 'tel:+48516215523',
+    email: 'twinss.group@gmail.com',
+    emailHref: 'mailto:twinss.group@gmail.com',
+    location: 'Lublin i okolice',
+    company: 'Bartłomiej Karpiński, NIP: 5050074581, REGON: 367733735'
+};
