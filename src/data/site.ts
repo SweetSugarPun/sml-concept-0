59275b8e9a5ee635b07bc1957b07290b316d86f1
Arkadiusz Sugier <sugier.arkadiusz@gmail.com>
2026-06-01 19:25:33 +0200

    Changes before first release

---
+++ src/data/site.ts
@@ -0,0 +1,22 @@
+export const faqItems = [
+    {
+        question: 'Czy skupujecie mieszkania zadłużone lub z hipoteką?',
+        answer:
+            'Tak, analizujemy również sprawy z długami, hipoteką czy komornikiem. Na podstawie dokumentów przedstawiamy realną ofertę z uwzględnieniem zadłużenia.',
+    },
+    {
+        question: 'Ile czasu zajmuje cały proces sprzedaży?',
+        answer:
+            'W prostych przypadkach formalności można zamknąć nawet w ciągu kilku dni. Termin aktu notarialnego zależy też od dostępności notariusza i skomplikowania sprawy.',
+    },
+    {
+        question: 'Jakie dokumenty są potrzebne do wyceny?',
+        answer:
+            'Najczęściej potrzebna jest księga wieczysta, podstawa nabycia mieszkania (np. akt notarialny, postanowienie o nabyciu spadku) oraz dowód osobisty właściciela. Przy zadłużeniu przydadzą się także pisma od wierzycieli.',
+    },
+    {
+        question: 'Czy muszę przygotowywać mieszkanie do sprzedaży?',
+        answer:
+            'Nie wymagamy remontów ani homestagingu. Kupujemy mieszkania w stanie obecnym, również do generalnego remontu.',
+    },
+];
